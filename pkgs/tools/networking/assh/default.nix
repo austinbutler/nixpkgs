9edf57665d8a07786efb73ddd3e1d54c17289953
Austin Butler <abutler@riotgames.com>
2022-01-03 11:27:23 -0800

    assh: fix build on Darwin

--- pkgs/tools/networking/assh/default.nix
+++ pkgs/tools/networking/assh/default.nix
@@ -1,5 +1,6 @@
 { lib
 , buildGoModule
+, fetchpatch
 , fetchFromGitHub
 , openssh
 , makeWrapper
@@ -12,11 +13,11 @@ buildGoModule rec {
   src = fetchFromGitHub {
     repo = "advanced-ssh-config";
     owner = "moul";
-    rev = "v${version}";
-    sha256 = "sha256-FqxxNTsZVmCsIGNHRWusFP2gba2+geqBubw+6PeR75c=";
+    rev = "19cf2fc158ecb532679442558f32c426d92373c5";
+    sha256 = "1323ck1wmdhzx9903dw7ignzylb739rxqfcay0cvi9l9cdipchdr";
   };
 
-  vendorSha256 = "sha256-AYBwuRSeam5i2gex9PSG9Qk+FHdEhIpY250CJo01cFE=";
+  vendorSha256 = "0d2202jgaklrwx773cff4lryhm1dbah80lcinxrnf3qd182vbhcp";
 
   doCheck = false;
 
@@ -25,6 +26,15 @@ buildGoModule rec {
   ];
 
   nativeBuildInputs = [ makeWrapper ];
+
+  #patches = [
+  #  # https://github.com/moul/assh/pull/430
+  #  (fetchpatch {
+  #    name = "gopsutil-update-1";
+  #    url = "https://github.com/moul/assh/commit/19cf2fc158ecb532679442558f32c426d92373c5.patch";
+  #    sha256 = "0a089ay6sk4ixgx46fdp7pz86fk8nzi4zql20b6c2w3z80hh5c1g";
+  #  })
+  #];
 
   postInstall = ''
     wrapProgram "$out/bin/assh" \
