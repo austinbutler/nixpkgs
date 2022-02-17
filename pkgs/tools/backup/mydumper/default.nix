ec7c81b2a078b2c3a05c365f742533b4d06480e1
Austin Butler <abutler@riotgames.com>
2022-02-17 12:18:44 -0800

    WIP

--- pkgs/tools/backup/mydumper/default.nix
+++ pkgs/tools/backup/mydumper/default.nix
@@ -1,30 +1,36 @@
 { lib, stdenv, fetchFromGitHub, cmake, pkg-config
-, glib, zlib, pcre, libmysqlclient, libressl }:
+, glib, zlib, pcre, mysql80, openssl }:
 
 let inherit (lib) getDev; in
 
 stdenv.mkDerivation rec {
-  version = "0.9.5";
+  version = "0.11.5-2";
   pname = "mydumper";
 
   src = fetchFromGitHub {
-    owner  = "maxbube";
+    owner  = "mydumper";
     repo   = "mydumper";
     rev    = "v${version}";
-    sha256 = "0vbz0ri5hm6yzkrcgnaj8px6bf59myr5dbhyy7fd4cv44hr685k6";
+    sha256 = "0hwcas689hbf5a6qvan0ccjfwys26d1vzgq62vp69v6qjwfbgpkz";
   };
 
   nativeBuildInputs = [ cmake pkg-config ];
 
-  buildInputs = [ glib zlib pcre libmysqlclient libressl ];
+  buildInputs = [ glib zlib pcre mysql80 openssl ];
 
-  cmakeFlags = [ "-DMYSQL_INCLUDE_DIR=${getDev libmysqlclient}/include/mysql" ];
+  # prePatch = ''
+  #   echo "DEV: ${getDev mysql80}"
+  #   exit 1
+  # '';
+
+  # cmakeFlags = [ "-DMYSQL_INCLUDE_DIR=${mysql80}/lib" ];
+  cmakeFlags = [ "-DBUILD_DOCS=OFF" "-DWITH_ZSTD=OFF" "-Wno-dev" ];
 
   meta = with lib; {
     description = "High-perfomance MySQL backup tool";
-    homepage = "https://github.com/maxbube/mydumper";
+    homepage = "https://github.com/mydumper/mydumper";
     license = licenses.gpl3;
-    platforms = platforms.linux;
+    platforms = platforms.unix;
     maintainers = with maintainers; [ izorkin ];
   };
 }
