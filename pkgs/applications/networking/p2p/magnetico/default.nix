bc8e52ca0e848f13338747f5e46a7b7063f40df9
zowoq <59103226+zowoq@users.noreply.github.com>
2022-10-25 09:37:43 +1000

    magnetico: unpin go 1.17

--- pkgs/applications/networking/p2p/magnetico/default.nix
+++ pkgs/applications/networking/p2p/magnetico/default.nix
@@ -11,6 +11,8 @@ buildGoModule rec {
     sha256 = "1avqnfn4llmc9xmpsjfc9ivki0cfvd8sljfzd9yac94xcj581s83";
   };
 
+  # package still builds but the vendor isn't reproducible with go > 1.17: nix-build -A $name.go-modules --check
+  # may end up needing to mark the package as broken
   vendorSha256 = "087kikj6sjhjxqymnj7bpxawfmwckihi6mbmi39w0bn2040aflx5";
 
   nativeBuildInputs = [ go-bindata ];
