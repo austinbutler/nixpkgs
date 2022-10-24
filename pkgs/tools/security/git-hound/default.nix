aa9742a2916712303a0d8bf611aae26da7f442cc
zowoq <59103226+zowoq@users.noreply.github.com>
2022-10-25 09:37:48 +1000

    git-hound: unpin go 1.17

--- pkgs/tools/security/git-hound/default.nix
+++ pkgs/tools/security/git-hound/default.nix
@@ -1,6 +1,7 @@
 { buildGoModule
 , fetchFromGitHub
 , lib
+, stdenv
 }:
 
 buildGoModule rec {
@@ -14,6 +15,8 @@ buildGoModule rec {
     sha256 = "sha256-HD5OK8HjnLDbyC/TmVI2HfBRIUCyyHTbA3JvKoeXV5E=";
   };
 
+  # package still builds but the vendor isn't reproducible with go > 1.17: nix-build -A $name.go-modules --check
+  # may end up needing to mark the package as broken
   vendorSha256 = "sha256-qnIcjk2mzG/51ouhrAW6R3ZqoUSL6ZzYCOVZvKS7sBQ=";
 
   meta = with lib; {
@@ -26,5 +29,6 @@ buildGoModule rec {
     homepage = "https://github.com/tillson/git-hound";
     license = with licenses; [ mit ];
     maintainers = with maintainers; [ fab ];
+    broken = stdenv.isDarwin; # build fails with go > 1.17
   };
 }
