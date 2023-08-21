53f497a9d894f21bfdb59551437e9ab73ba1e658
Austin Butler <austinabutler@gmail.com>
2023-08-20 23:49:03 -0700

    WIP

--- pkgs/development/python-modules/awslambdaric/default.nix
+++ pkgs/development/python-modules/awslambdaric/default.nix
@@ -16,27 +16,27 @@
 
 buildPythonPackage rec {
   pname = "awslambdaric";
-  version = "2.0.0";
+  version = "2.0.5";
   disabled = isPy27;
 
   src = fetchFromGitHub {
     owner = "aws";
     repo = "aws-lambda-python-runtime-interface-client";
     rev = version;
-    sha256 = "1amlaq119mk8fa3fxi3d6vgp83vcd81mbk53jzbixacklmcsp50k";
+    sha256 = "sha256-MUCuNkt4P1nnDBIcqai7uSUy2SFu6BNPqxBVop6FFf8=";
   };
 
-  patches = [
-    (fetchpatch {
-      # https://github.com/aws/aws-lambda-python-runtime-interface-client/pull/58
-      url = "https://github.com/aws/aws-lambda-python-runtime-interface-client/commit/162c3c0051bb9daa92e4a2a4af7e90aea60ee405.patch";
-      sha256 = "09qqq5x6npc9jw2qbhzifqn5sqiby4smiin1aw30psmlp21fv7j8";
-    })
-  ];
+  # patches = [
+  #   (fetchpatch {
+  #     # https://github.com/aws/aws-lambda-python-runtime-interface-client/pull/58
+  #     url = "https://github.com/aws/aws-lambda-python-runtime-interface-client/commit/162c3c0051bb9daa92e4a2a4af7e90aea60ee405.patch";
+  #     sha256 = "09qqq5x6npc9jw2qbhzifqn5sqiby4smiin1aw30psmlp21fv7j8";
+  #   })
+  # ];
 
   postPatch = ''
     substituteInPlace requirements/base.txt \
-      --replace 'simplejson==3' 'simplejson~=3'
+      --replace 'simplejson==3.17.2' 'simplejson>=3'
   '';
 
   propagatedBuildInputs = [ simplejson ];
@@ -51,13 +51,13 @@ buildPythonPackage rec {
 
   disabledTests = [
     # Test fails with: Assertion error
-    "test_handle_event_request_fault_exception_logging_syntax_error"
+    # "test_handle_event_request_fault_exception_logging_syntax_error"
   ];
 
   pythonImportsCheck = [ "awslambdaric" "runtime_client" ];
 
   meta = with lib; {
-    broken = (stdenv.isLinux && stdenv.isAarch64);
+    broken = stdenv.isLinux && stdenv.isAarch64;
     description = "AWS Lambda Runtime Interface Client for Python";
     homepage = "https://github.com/aws/aws-lambda-python-runtime-interface-client";
     license = licenses.asl20;
